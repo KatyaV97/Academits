5662480e822aa1466f37e349c933bf97db31fec8
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2020-01-28 21:55:39 +0700

    Разработала проект Range.

---
+++ CourseTasks/Range/Range.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+	class Range
+	{
+		public double From
+		{
+			get;
+			set;
+		}
+
+		public double To
+		{
+			get;
+			set;
+		}
+
+		public Range(double from, double to)
+		{
+			From = from;
+			To = to;
+		}
+
+		public double GetLength()
+		{
+			return To - From;
+		}
+
+		public bool IsInside(double x)
+		{
+			return x >= From && x <= To;
+		}
+	}
+}
---
+++ CourseTasks/Range/UseClassRange.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+	class UseClassRange
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите начальное значение диапазона: ");
+			double from = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Введите начальное значение диапазона: ");
+			double to = Convert.ToDouble(Console.ReadLine());
+
+			Range range = new Range(from, to);
+
+			Console.WriteLine("Длина заданного диапазона: " + range.GetLength());
+
+			Console.WriteLine("Введите число: ");
+			double x = Convert.ToDouble(Console.ReadLine());
+
+			if (range.IsInside(x))
+			{
+				Console.WriteLine("Введенное число находится в указанном диапазоне.");
+			}
+			else
+			{
+				Console.WriteLine("Введенное число не находится в указанном диапазоне.");
+			}
+
+			Console.ReadKey();
+		}
+
+	}
+}
