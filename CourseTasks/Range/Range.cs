380e1cf6d05a43e3d247926f6fbb131d57e2f427
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2020-02-04 22:20:06 +0700

    Доработала класс Range. Добавила функции персечения, объединения, разности диапазонов. Использовала новые функции в классе UseClassRange.

--- CourseTasks/Range/Range.cs
+++ CourseTasks/Range/Range.cs
@@ -23,9 +23,93 @@ namespace Range
 			return To - From;
 		}
 
-		public bool IsInside(double x)
+		public double[] GetIntersection(double otherFrom, double otherTo)
 		{
-			return x >= From && x <= To;
+			double[] range = new double[] { otherFrom, otherTo };
+
+			if (From > range[1] || To < range[0])
+			{
+				return null;
+			}
+
+			if (From > range[0])
+			{
+				range[0] = From;
+			}
+
+			if (To < range[1])
+			{
+				range[1] = To;
+			}
+
+			return range;
+		}
+
+		public double[] GetUnion(double otherFrom, double otherTo)
+		{
+			double[] range = new double[4] { otherFrom, otherTo, 0, 0 };
+
+			if (From > range[1] || To < range[0])
+			{
+				range[0] = From;
+				range[1] = To;
+				range[2] = otherFrom;
+				range[3] = otherTo;
+				return range;
+			}
+
+			if (From < range[0])
+			{
+				range[0] = From;
+			}
+
+			if (To > range[1])
+			{
+				range[1] = To;
+			}
+
+			return range;
+		}
+
+		public double[] GetDifference(double otherFrom, double otherTo)
+		{
+			double[] range = new double[4] { otherFrom, otherTo, 0, 0 };
+
+			if (From > range[1] || To < range[0])
+			{
+				range[0] = From;
+				range[1] = To;
+
+				return range;
+			}
+
+			if (range[0] < From && range[1] > To)
+			{
+				return null;
+			}
+
+			if (range[0] > From && range[1] < To)
+			{
+				range[0] = From;
+				range[1] = otherFrom;
+				range[2] = otherTo;
+				range[3] = To;
+
+				return range;
+			}
+
+			if (range[0] < From && range[1] < To)
+			{
+				range[0] = range[1];
+				range[1] = To;
+
+				return range;
+			}
+
+			range[0] = From;
+			range[1] = range[0];
+
+			return range;
 		}
 	}
 }
--- CourseTasks/Range/UseClassRange.cs
+++ CourseTasks/Range/UseClassRange.cs
@@ -10,26 +10,72 @@ namespace Range
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Введите начальное значение диапазона: ");
-			double from = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите начальное значение первого диапазона: ");
+			double from1 = Convert.ToDouble(Console.ReadLine());
 
-			Console.WriteLine("Введите конечное значение диапазона: ");
-			double to = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Введите конечное значение первого диапазона: ");
+			double to1 = Convert.ToDouble(Console.ReadLine());
 
-			Range range = new Range(from, to);
-			
-			Console.WriteLine("Длина заданного диапазона: " + range.GetLength());
+			Range range1 = new Range(from1, to1);
 
-			Console.WriteLine("Введите число: ");
-			double x = Convert.ToDouble(Console.ReadLine());
+			Console.WriteLine("Длина заданного диапазона: " + range1.GetLength());
 
-			if (range.IsInside(x))
+			Console.WriteLine(Environment.NewLine + "Введите начальное значение второго диапазона: ");
+			double from2 = Convert.ToDouble(Console.ReadLine());
+
+			Console.WriteLine("Введите конечное значение второго диапазона: ");
+			double to2 = Convert.ToDouble(Console.ReadLine());
+
+			Range range2 = new Range(from2, to2);
+
+			Console.WriteLine("Длина заданного диапазона: " + range2.GetLength());
+
+			Console.WriteLine(Environment.NewLine + "Результат пересечения диапазонов:");
+
+			if (range1.GetIntersection(from2, to2) == null)
 			{
-				Console.WriteLine("Введенное число находится в указанном диапазоне.");
+				Console.WriteLine("Второй диапазон не пересекается с первым.");
 			}
 			else
 			{
-				Console.WriteLine("Введенное число не находится в указанном диапазоне.");
+				double[] intersectionRange = range1.GetIntersection(from2, to2);
+
+				Console.WriteLine("Новый диапазон от " + intersectionRange[0] + " до " + intersectionRange[1] + ".");
+			}
+
+			double[] unionRange = range1.GetUnion(from2, to2);
+
+			Console.WriteLine(Environment.NewLine + "Результат объединения диапазонов:");
+
+			if (unionRange[2] == 0 && unionRange[3] == 0)
+			{
+				Console.WriteLine("Новый диапазон от " + unionRange[0] + " до " + unionRange[1] + ".");
+			}
+			else
+			{
+				Console.Write("Новый диапазон: от " + unionRange[0] + " до " + unionRange[1] + " и ");
+				Console.WriteLine("от " + unionRange[2] + " до " + unionRange[3] + ".");
+			}
+
+			Console.WriteLine(Environment.NewLine + "Результат разности диапазонов:");
+
+			if (range1.GetDifference(from2, to2) == null)
+			{
+				Console.WriteLine("Первый диапазон полностью вычитается из второго.");
+			}
+			else
+			{
+				double[] differenceRange = range1.GetDifference(from2, to2);
+
+				if (differenceRange[2] == 0 && differenceRange[3] == 0)
+				{
+					Console.WriteLine("Новый диапазон от " + differenceRange[0] + " до " + differenceRange[1] + ".");
+				}
+				else
+				{
+					Console.Write("Новый диапазон: от " + differenceRange[0] + " до " + differenceRange[1] + " и ");
+					Console.WriteLine("от " + differenceRange[2] + " до " + differenceRange[3] + ".");
+				}
 			}
 
 			Console.ReadKey();
