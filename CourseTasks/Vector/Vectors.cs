7e38f32e81531cf7e419b615dc42270211fa4101
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2020-02-26 21:44:22 +0700

    Разработала класс вектор

---
+++ CourseTasks/Vector/Vectors.cs
@@ -0,0 +1,207 @@
+﻿using System;
+
+namespace Vectors
+{
+	class Vectors
+	{
+		public int N { get; set; }
+
+		//public Vectors Vector { get; set; }
+
+		public double[] Vector { get; set; }
+
+		public Vectors(int n)
+		{
+			if (n <= 0)
+			{
+				throw new ArgumentException("n должен быть > 0", nameof(n));
+			}
+
+			N = n;
+
+			this.Vector = new double[N];
+		}
+
+		public Vectors(Vectors vector) { this.Vector = vector.Vector; }
+
+		public Vectors(double[] vectorsValues)
+		{
+			this.Vector = vectorsValues;
+		}
+
+		public Vectors(int n, double[] vectorsValues)
+		{
+			if (n <= 0)
+			{
+				throw new ArgumentException("n должен быть > 0", nameof(n));
+			}
+
+			N = n;
+
+			this.Vector = new double[N];
+
+			int count = 0;
+
+			foreach (int e in vectorsValues)
+			{
+				Vector[count] = e;
+				count++;
+			}
+
+			if (N > vectorsValues.Length)
+			{
+				for (int i = vectorsValues.Length; i < N; i++)
+				{
+					Vector[i] = 0;
+				}
+			}
+		}
+
+		public int GetSize()
+		{
+			return Vector.Length;
+		}
+
+		public override string ToString()
+		{
+			return "{" + string.Join(",", Vector) + "}";
+		}
+
+		public void AdditionVectors(Vectors vector)
+		{
+			int vectorMaxSize = Math.Max(GetSize(), vector.GetSize());
+			int vectorMinSize = Math.Min(GetSize(), vector.GetSize());
+
+			for (int i = 0; i < vectorMinSize; i++)
+			{
+				Vector[i] += vector.Vector[i];
+			}
+
+			if (GetSize() > vector.GetSize())
+			{
+				for (int i = vectorMinSize; i < vectorMaxSize; i++)
+				{
+					Vector[i] = vector.Vector[i];
+				}
+			}
+			else if (vector.GetSize() > GetSize())
+			{
+				for (int i = vectorMinSize; i < vectorMaxSize; i++)
+				{
+					Vector[i] = vector.Vector[i];
+				}
+			}
+		}
+
+		public void SubtractionVectors(Vectors vector)
+		{
+			int vectorMaxSize = Math.Max(GetSize(), vector.GetSize());
+			int vectorMinSize = Math.Min(GetSize(), vector.GetSize());
+
+			for (int i = 0; i < vectorMinSize; i++)
+			{
+				Vector[i] = Vector[i] - vector.Vector[i];
+			}
+
+			if (vector.GetSize() > GetSize())
+			{
+				for (int i = vectorMinSize; i < vectorMaxSize; i++)
+				{
+					Vector[i] = 0 - vector.Vector[i];
+				}
+			}
+		}
+
+		public void MultiplyVectorByScalar(double scalar)
+		{
+			for (int i = 0; i < GetSize(); i++)
+			{
+				Vector[i] *= scalar;
+			}
+		}
+
+		public void ReverseVector()
+		{
+			for (int i = 0; i < GetSize(); i++)
+			{
+				Vector[i] *= -1;
+			}
+		}
+
+		public double GetLength()
+		{
+			double squaresСoordinatesSum = 0;
+
+			for (int i = 0; i < GetSize(); i++)
+			{
+				squaresСoordinatesSum += Math.Pow(Vector[i], 2);
+			}
+
+			return Math.Sqrt(squaresСoordinatesSum);
+		}
+
+		public static Vectors AddVectors(Vectors vector1, Vectors vector2)
+		{
+			int vectorMaxSize = Math.Max(vector1.GetSize(), vector2.GetSize());
+			int vectorMinSize = Math.Min(vector1.GetSize(), vector2.GetSize());
+
+			double[] additionResult = new double[vectorMaxSize];
+
+			for (int i = 0; i < vectorMinSize; i++)
+			{
+				additionResult[i] = vector1.Vector[i] + vector2.Vector[i];
+			}
+
+			if (vector1.GetSize() > vector2.GetSize())
+			{
+				for (int i = vectorMinSize; i < vectorMaxSize; i++)
+				{
+					additionResult[i] = vector1.Vector[i];
+				}
+			}
+			else if (vector2.GetSize() > vector1.GetSize())
+			{
+				for (int i = vectorMinSize; i < vectorMaxSize; i++)
+				{
+					additionResult[i] = vector2.Vector[i];
+				}
+			}
+
+			Vectors newVector = new Vectors(additionResult);
+
+			return newVector;
+		}
+
+		public static Vectors SubtractVectors(Vectors vector1, Vectors vector2)
+		{
+			int vectorMaxSize = Math.Max(vector1.GetSize(), vector2.GetSize());
+			int vectorMinSize = Math.Min(vector1.GetSize(), vector2.GetSize());
+
+			double[] subtractionResult = new double[vectorMaxSize];
+
+			for (int i = 0; i < vectorMinSize; i++)
+			{
+				subtractionResult[i] = vector1.Vector[i] - vector2.Vector[i];
+			}
+
+			if (vector1.GetSize() > vector2.GetSize())
+			{
+				for (int i = vectorMinSize; i < vectorMaxSize; i++)
+				{
+					subtractionResult[i] = vector1.Vector[i];
+				}
+			}
+			else if (vector2.GetSize() > vector1.GetSize())
+			{
+				for (int i = vectorMinSize; i < vectorMaxSize; i++)
+				{
+					subtractionResult[i] = 0 - vector2.Vector[i];
+				}
+			}
+
+			Vectors newVector = new Vectors(subtractionResult);
+
+			return newVector;
+		}
+	}
+}
---
+++ CourseTasks/Vector/VectorsProgram.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Vectors
+{
+	class VectorsProgram
+	{
+		static void Main(string[] args)
+		{
+			Vectors vector1 = new Vectors(new double[] { 10, 20, 30 });
+			Vectors vector2 = new Vectors(new double[] { 15, 5, 45 });
+
+			Vectors additionalVectors = Vectors.AddVectors(vector1, vector2);
+
+			Console.WriteLine("Результат сложения векторов статическим методом: " + additionalVectors);
+
+			vector1.AdditionVectors(vector2);
+
+			Console.WriteLine("Результат сложения векторов нестатическим методом: " + vector1);
+
+			Vectors subtractionalVectors = Vectors.SubtractVectors(vector1, vector2);
+
+			Console.WriteLine("Результат вычитания векторов статическим методом: " + subtractionalVectors);
+
+			vector1.SubtractionVectors(vector2);
+
+			Console.WriteLine("Результат вычитания векторов статическим методом: " + vector1);
+
+			vector1.MultiplyVectorByScalar(12);
+
+			Console.WriteLine("Результат усножения вектора на скаляр: " + vector1);
+
+			vector1.ReverseVector();
+
+			Console.WriteLine("Разворот вектора: " + vector1);
+
+			double vector1Length = vector1.GetLength();
+
+			Console.WriteLine("Длина вектора: " + vector1Length);
+
+			Vectors vector3 = new Vectors(6);
+			Vectors vector4 = new Vectors(6, new double[] { 3, 4, 9, 6, 7, 2 });
+			Vectors vector5 = new Vectors(vector4);
+
+			Console.WriteLine("Вектор №3: " + vector3);
+			Console.WriteLine("Вектор №4: " + vector4);
+			Console.WriteLine("Вектор №5: " + vector5);
+
+			Console.ReadKey();
+		}
+	}
+}
