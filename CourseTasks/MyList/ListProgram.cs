fe308d9aef50110f674d7dbd790bacd81ba1bcb2
ekaterinavasileva01 <ekaterinavasileva01@gmail.com>
2020-03-29 21:16:39 +0700

    Разработала класс List

---
+++ CourseTasks/MyList/ListProgram.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyList
+{
+	class ListProgram
+	{
+		static void Main()
+		{
+			var customers = new MyList<string>();
+
+			customers.Add("Игорь");
+			customers.Add("Александр");
+			customers.Add("Иван");
+
+			Console.WriteLine("Первые покупатели: " + customers);
+			Console.WriteLine(Environment.NewLine);
+
+			var customersPerDayCount = new MyList<int>();
+
+			customersPerDayCount.Add(25);
+			customersPerDayCount.Add(55);
+			customersPerDayCount.Add(85);
+			customersPerDayCount[3] = 36;
+			customersPerDayCount[4] = 42;
+			customersPerDayCount[5] = 3;
+			customersPerDayCount[6] = 24;
+			customersPerDayCount[7] = 24;
+			customersPerDayCount[8] = 24;
+			customersPerDayCount[9] = 24;
+			customersPerDayCount[10] = 6;
+			customersPerDayCount[11] = 8;
+
+			Console.WriteLine("Количество покупателей в день: " + customersPerDayCount);
+			Console.WriteLine("Вместимость: " + customersPerDayCount.Capacity);
+			Console.WriteLine("Количество заполненных дней: " + customersPerDayCount.Count);
+			Console.WriteLine(Environment.NewLine);
+
+			customersPerDayCount.TrimExcess();
+
+			Console.WriteLine("Количество покупателей в день: " + customersPerDayCount);
+			Console.WriteLine("Вместимость: " + customersPerDayCount.Capacity);
+			Console.WriteLine("Количество заполненных дней: " + customersPerDayCount.Count);
+			Console.WriteLine(Environment.NewLine);
+
+			customersPerDayCount.Clear();
+
+			customersPerDayCount[0] = 36;
+			customersPerDayCount[1] = 42;
+			customersPerDayCount[2] = 52;
+
+			if (customersPerDayCount.Contains(0))
+			{
+				Console.WriteLine("Есть дни с количеством покупателей = 0.");
+			}
+			else
+			{
+				Console.WriteLine("Нет дней с количеством покупателей = 0.");
+			}
+
+			Console.WriteLine(Environment.NewLine);
+
+			
+			int[] customesrCount = new int[customersPerDayCount.Count];
+
+			customersPerDayCount.CopyTo(customesrCount, 0);
+
+			Console.WriteLine("Количество покупателей: ");
+
+			foreach (int e in customesrCount)
+			{
+				Console.Write(e + " ");
+			}
+			Console.WriteLine(Environment.NewLine);
+
+			customers.Insert(customers.IndexOf("Александр"), "Саша");
+
+			Console.WriteLine("Первые покупатели: " + customers);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ CourseTasks/MyList/MyList.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyList
+{
+	class MyList<T> : IList<T>
+	{
+		private T[] items;
+		private int modCount;
+
+		public bool IsReadOnly => false;
+
+		public int Count { get; private set; }
+
+		public int  Capacity { get; set; }
+
+		public T this[int index]
+		{
+			get
+			{
+				if (index > Count)
+				{
+					throw new IndexOutOfRangeException("Индекс = " + index + " должен быть > 0 и < " +
+						Count);
+				}
+
+				return items[index];
+			}
+
+			set
+			{
+				if (index > Count)
+				{
+					throw new IndexOutOfRangeException("Индекс = " + index + " должен быть > 0 и < " +
+						Count);
+				}
+
+				if (Count >= Capacity)
+				{
+					IncreaseCapacity();
+				}
+
+				items[index] = value;
+				Count++;
+			}
+		}
+
+		public MyList()
+		{
+			Count = 0;
+			Capacity = 10;
+
+			items =  new T[Capacity];
+		}
+
+		public MyList(int capacity)
+		{
+			Capacity = capacity;
+
+			items = new T[Capacity];
+		}
+
+		public void TrimExcess()
+		{
+			if (Count == Capacity)
+			{
+				return;
+			}
+
+			Capacity = Count;
+			Array.Resize(ref items, Capacity);
+		}
+
+		private void IncreaseCapacity()
+		{
+			T[] oldItems = items;
+			Capacity *= 2;
+
+			items = new T[Capacity];
+			Array.Copy(oldItems, items, Count);
+		}
+
+		public void Add(T item)
+		{
+			if (Count >= Capacity)
+			{
+				IncreaseCapacity();
+			}
+
+			items[Count] = item;
+
+			Count++;
+			modCount++;
+		}
+
+		public void Clear()
+		{
+			Count = 0;
+
+			modCount++;
+		}
+
+		public bool Contains(T item)
+		{
+			for (int i = 0; i < Count; i++)
+			{
+				if (items[i].Equals(item))
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public void CopyTo(T[] array, int arrayIndex)
+		{
+			for (int i = arrayIndex; i < Count; i++)
+			{
+				array.SetValue(items[i], arrayIndex++);
+			}
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			int oldModCount = modCount;
+
+			for (int i = 0; i < Count; i++)
+			{
+				if (oldModCount != modCount)
+				{
+					throw new InvalidOperationException("Список был изменен после создания перечисления.");
+				}
+
+				yield return items[i];
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public int IndexOf(T item)
+		{
+			for (int i = 0; i < Count; i++)
+			{
+				if (items[i].Equals(item))
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+
+		public void Insert(int index, T item)
+		{
+			if (index >= Count)
+			{
+				throw new IndexOutOfRangeException("Индекс = " + index + " должен быть > 0 и < " +
+					Count);
+			}
+
+			items[index] = item;
+
+			modCount++;
+		}
+
+		public bool Remove(T item)
+		{
+			for (int i = 0; i < Count; i++)
+			{
+				if (items[i].Equals(item))
+				{
+					Count--;
+					modCount++;
+
+					return true;
+				}
+			}
+
+			return false;
+		}
+
+		public void RemoveAt(int index)
+		{
+			if (index >= Count)
+			{
+				throw new IndexOutOfRangeException("Индекс = " + index + " должен быть > 0 и < " +
+					Count);
+			}
+
+			for (int i = index; i < Count - 1; i++)
+			{
+				items[i] = items[i + 1];
+			}
+
+			Count--;
+			modCount++;
+		}
+
+		public override string ToString()
+		{
+			StringBuilder printResult = new StringBuilder();
+			printResult.Append("[");
+
+			for (int i = 0; i < Count - 1; i++)
+			{
+				printResult.Append(items[i] + " , ");
+			}
+
+			printResult.Append(items[Count - 1] + "]");
+
+			return printResult.ToString();
+		}
+	}
+}
