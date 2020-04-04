617abe32447a9a19e149fea934989916dcc29730
KatyaV97 <ekaterinavasileva01@gmail.com>
2020-04-04 17:42:33 +0700

    Исправила String на string, везде тип переменных поставила var, исправила подзадачу (г).

--- CourseTasks/Lambdas/Lambdas.cs
+++ CourseTasks/Lambdas/Lambdas.cs
@@ -26,8 +26,8 @@ namespace Lambdas
 				.OrderBy(p => p)
 				.ToList();
 
-			Console.WriteLine(String.Join(", ", sortedUniqueNamesPersons));
-			Console.WriteLine("Имена: " + String.Join(", ", sortedUniqueNamesPersons) + ".");
+			Console.WriteLine(string.Join(", ", sortedUniqueNamesPersons));
+			Console.WriteLine("Имена: " + string.Join(", ", sortedUniqueNamesPersons) + ".");
 			Console.WriteLine(Environment.NewLine);
 
 			var filteredPersons = persons
@@ -38,25 +38,25 @@ namespace Lambdas
 				.OrderBy(p => p)
 				.ToList();
 
-			double filteredPersonsAverageAge = filteredPersons
+			var filteredPersonsAverageAge = filteredPersons
 				.Select(f => f.Age)
 				.Average();
 
-			Console.WriteLine("Имена людей с возрастом < 18 лет: " + String.Join(", ", filteredPersonsNames));
+			Console.WriteLine("Имена людей с возрастом < 18 лет: " + string.Join(", ", filteredPersonsNames));
 			Console.WriteLine("Средний возраст людей с возрастом < 18 лет: " + filteredPersonsAverageAge);
 			Console.WriteLine(Environment.NewLine);
 
 			var personsByName = persons
 				.GroupBy(p => p.Name, p => p.Age)
 				.OrderBy(p => p.Key)
-				.ToDictionary(p => p.Key, p => p.ToList().Average());
+				.ToDictionary(p => p.Key, p => p.Average());
 
-			int personNameMaxLength = persons
+			var personNameMaxLength = persons
 				.Max(p => p.Name.Length);
 
 			foreach (KeyValuePair<string, double> kvp in personsByName)
 			{
-				Console.WriteLine("Имя: " + kvp.Key.ToString().PadRight(personNameMaxLength) + " ; Средний возраст: " + kvp.Value);
+				Console.WriteLine("Имя: " + kvp.Key.PadRight(personNameMaxLength) + " Средний возраст: " + kvp.Value);
 			}
 
 			Console.WriteLine(Environment.NewLine);
@@ -66,7 +66,7 @@ namespace Lambdas
 				.OrderByDescending(p => p.Age)
 				.Select(p => p.Name);
 
-			Console.WriteLine("Имена людей с возрастом от 20 до 45: " + String.Join(", ", filteredNamesPersons));
+			Console.WriteLine("Имена людей с возрастом от 20 до 45: " + string.Join(", ", filteredNamesPersons));
 
 			Console.ReadKey();
 		}
